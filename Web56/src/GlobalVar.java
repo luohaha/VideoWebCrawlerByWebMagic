dc9a51ea21775bd21a7d5b56547cdfb0f9767dc5
Yixin Luo <18810541851@163.com>
2015-10-10 11:26:25 +0800

    添加信息
    添加GlobalVar.java的说明信息

--- Web56/src/GlobalVar.java
+++ Web56/src/GlobalVar.java
@@ -1,6 +1,10 @@
 import java.util.HashMap;
 import java.util.Map;
-
+/*
+ * suthor : Yixin Luo
+ * date : 2015/10/9
+ * description : 用来保存不同网站的视频数量，是一个全局的map
+ * */
 public class GlobalVar {
 	public static Map<String, Double> count = new HashMap<String, Double>();
 }
